49d1ebdf582688e374ccc0c3a9ca46293d56e975
SoniEx2 <endermoneymod@gmail.com>
2013-06-17 15:38:00 -0300

    Made traders work... sort of... :/
    TODO: Make the inventory actually update

--- common/com/github/soniex2/endermoney/trading/helper/item/ItemStackMapKey.java
+++ common/com/github/soniex2/endermoney/trading/helper/item/ItemStackMapKey.java
@@ -12,23 +12,28 @@ public class ItemStackMapKey {
 	public ItemStackMapKey(ItemStack is) {
 		this.itemID = is.itemID;
 		this.damage = is.getItemDamage();
-		this.tag = is.stackTagCompound;
+		this.tag = (NBTTagCompound) (is.stackTagCompound != null ? is.stackTagCompound.copy()
+				: null);
 	}
-	
+
 	public ItemStackMapKey(int itemID, int damage, NBTTagCompound tagCompound) {
 		this.itemID = itemID;
 		this.damage = damage;
-		this.tag = tagCompound;
+		this.tag = (NBTTagCompound) (tagCompound != null ? tagCompound.copy() : null);
 	}
 
 	public boolean equals(Object obj) {
-		if (!(obj instanceof ItemStackMapKey))
-			return false;
+		if (!(obj instanceof ItemStackMapKey)) return false;
 		return itemID == ((ItemStackMapKey) obj).itemID
 				&& damage == ((ItemStackMapKey) obj).damage
-				&& tag.equals(((ItemStackMapKey) obj).tag);
+				&& (tag == null ? ((ItemStackMapKey) obj).tag == null : tag
+						.equals(((ItemStackMapKey) obj).tag));
 	}
-	
+
+	public int hashCode() {
+		return ((itemID & 32767) << 16 | (damage & 65536)) << 1 | (tag != null ? 1 : 0);
+	}
+
 	public NBTTagCompound getTag() {
 		return (NBTTagCompound) tag.copy();
 	}
