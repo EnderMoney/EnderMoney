1f6478b910359c607e0a4f337f2f9197d5a4bdd3
SoniEx2 <endermoneymod@gmail.com>
2013-07-24 15:12:58 -0300

    Removed unused import

--- common/com/github/soniex2/endermoney/core/item/EnderCoin.java
+++ common/com/github/soniex2/endermoney/core/item/EnderCoin.java
@@ -6,7 +6,6 @@ import java.util.Random;
 
 import com.github.soniex2.endermoney.core.EnderMoney;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
