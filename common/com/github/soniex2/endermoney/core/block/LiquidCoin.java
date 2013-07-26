85d7c28a78b94884a3a20710b23e25fa984664ab
SoniEx2 <endermoneymod@gmail.com>
2013-07-26 17:33:22 -0300

    Changed Liquid Money color

--- common/com/github/soniex2/endermoney/core/block/LiquidCoin.java
+++ common/com/github/soniex2/endermoney/core/block/LiquidCoin.java
@@ -2,8 +2,8 @@ package com.github.soniex2.endermoney.core.block;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.Icon;
@@ -14,21 +14,48 @@ import net.minecraftforge.fluids.Fluid;
 
 public class LiquidCoin extends BlockFluidClassic {
 
+	private Icon[] theIcon;
+
 	public LiquidCoin(int id, Fluid fluid) {
 		super(id, fluid, Material.water);
 		fluid.setBlockID(this);
-		this.setQuantaPerBlock(16);
+		this.setQuantaPerBlock(8);
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
 	public Icon getIcon(int side, int meta) {
-		return Block.waterMoving.getIcon(side, meta);
+		return side != 0 && side != 1 ? this.theIcon[1] : this.theIcon[0];
 	}
 
 	@Override
 	public int colorMultiplier(IBlockAccess iblockaccess, int x, int y, int z) {
-		return 0xFF0000;
+		int meta = iblockaccess.getBlockMetadata(x, y, z);
+		switch (meta) {
+			case 0:
+				return 0xffffff;
+			case 1:
+				return 0xf00000;
+			case 2:
+				return 0xff8000;
+			case 3:
+				return 0xffff00;
+			case 4:
+				return 0x7940;
+			case 5:
+				return 0x4040ff;
+			case 6:
+				return 0xa000c0;
+			case 7:
+				return 0;
+		}
+		return 0;
+	}
+
+	@Override
+	public void registerIcons(IconRegister ir) {
+		String l = "endermoneycore:liquid_";
+		this.theIcon = new Icon[] { ir.registerIcon(l + "still"), ir.registerIcon(l + "flow") };
 	}
 
 	@Override
