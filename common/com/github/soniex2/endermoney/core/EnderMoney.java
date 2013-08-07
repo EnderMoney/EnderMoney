b8f3df76377fbbb30d478053a25a49e902b83304
SoniEx2 <endermoneymod@gmail.com>
2013-08-07 15:50:17 -0300

    Removed line that wasn't supposed to be here

--- common/com/github/soniex2/endermoney/core/EnderMoney.java
+++ common/com/github/soniex2/endermoney/core/EnderMoney.java
@@ -32,7 +32,6 @@ import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-// @SuppressWarnings("unused")
 @Mod(modid = "EnderMoneyCore", name = "EnderMoney Core", version = Version.MOD_VERSION,
 		dependencies = "required-after:Forge")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
