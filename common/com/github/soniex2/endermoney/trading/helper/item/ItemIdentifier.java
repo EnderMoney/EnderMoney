969fa31b4f8d3ea0d715dd2a30b4539707bc5b14
SoniEx2 <EnderMoneyMod@gmail.com>
2014-02-27 18:15:18 -0300

    Better ItemIdentifier

--- common/com/github/soniex2/endermoney/trading/helper/item/ItemIdentifier.java
+++ common/com/github/soniex2/endermoney/trading/helper/item/ItemIdentifier.java
@@ -9,23 +9,27 @@ public class ItemIdentifier {
 	public final Item item;
 	public final int damage;
 	private final NBTTagCompound tag;
+	private int hashCode = 0;
+	private boolean hashCodeCached = false;
 
 	public ItemIdentifier(ItemStack is) {
 		this.item = is.getItem();
 		this.damage = is.getItemDamage();
-		this.tag = (NBTTagCompound) (is.stackTagCompound != null ? is.stackTagCompound.copy()
-				: null);
+		this.tag = (NBTTagCompound) (is.stackTagCompound != null ? is.stackTagCompound
+				.copy() : null);
 	}
 
 	public ItemIdentifier(Item item, int damage, NBTTagCompound tagCompound) {
 		this.item = item;
 		this.damage = damage;
-		this.tag = (NBTTagCompound) (tagCompound != null ? tagCompound.copy() : null);
+		this.tag = (NBTTagCompound) (tagCompound != null ? tagCompound.copy()
+				: null);
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (!(obj instanceof ItemIdentifier)) return false;
+		if (!(obj instanceof ItemIdentifier))
+			return false;
 		return item == ((ItemIdentifier) obj).item
 				&& damage == ((ItemIdentifier) obj).damage
 				&& (tag == null ? ((ItemIdentifier) obj).tag == null : tag
@@ -34,8 +38,12 @@ public class ItemIdentifier {
 
 	@Override
 	public int hashCode() {
-		return (item.getUnlocalizedName() + ":" + (damage & 65536) + (tag != null ? (":("
-				+ tag.toString() + ")") : "")).hashCode();
+		if (!hashCodeCached) {
+			hashCodeCached = true;
+			hashCode = item.getClass().getCanonicalName().hashCode()
+					+ (damage & 65535) + (tag != null ? tag.hashCode() : 0);
+		}
+		return hashCode;
 	}
 
 	public NBTTagCompound getTag() {
